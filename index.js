b8437292bc01caea6c8a3a664099ab52ee5d88ad
William Mosto <will.mosto@gmail.com>
2021-12-15 12:48:16 -0500

    Methods

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+class Bird {
+    constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+    }
+    speak(){
+        return this.sex === "male" ? `It's me! ${this.name}, the parrot!` : `${this.name} says squawk!`
+    }
+
+     // // if statement vs ternary operator // //
+
+    // speak(){
+    //   if(this.sex === "male"){
+    //     return `It's me! ${this.name}, the parrot!`
+    //   }
+    //   else{
+    //     return `${this.name} says squawk!`
+    //   }  
+    // }
+}
+class Cat {
+    constructor(name, sex){
+      this.name = name;
+      this.sex = sex;
+    }
+    speak(){
+      return `${this.name} says meow!`
+    }
+  }
+  
+  class Dog {
+    constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+    }
+    speak(){
+    return `${this.name} says woof!`
+    }
+  }
+  
